ee78f2e219dc849dc9d421ae42b574a9477d5b06
merry555 <jisukim8873@gmail.com>
2023-05-12 15:07:44 +0900

    translation v0.0.1

--- konta/tasks/machine_translation.py
+++ konta/tasks/machine_translation.py
@@ -80,7 +80,7 @@ class KoNTATranslation:
             **inputs.to(self.device), forced_bos_token_id=self.tokenizer.lang_code_to_id[self.LANG_ALIASES[tgt]], max_length=128
         )
 
-        output = self.tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)
+        output = self.tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
             
         return output
     
