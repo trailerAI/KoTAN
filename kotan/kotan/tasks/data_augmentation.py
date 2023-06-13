f3723a6b22d5b7d349633bccb3c009e55a8f8183
juhwanlee <juhwanlee@diquest.com>
2023-06-13 15:42:33 +0900

    reflect style transfer on Augmentation

--- kotan/kotan/tasks/data_augmentation.py
+++ kotan/kotan/tasks/data_augmentation.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
 
 class KoTANAugmentationFactory:
     """
@@ -20,12 +21,13 @@ class KoTANAugmentationFactory:
                  task, 
                  tgt, 
                  level,
-                 style
+                 style = None,
                  ):
         super().__init__()
         self.task = task
         self.tgt = tgt
         self.level = level
+        self.style = style
 
     def load(self, device):
         if self.level == "fine":
@@ -43,10 +45,21 @@ class KoTANAugmentationFactory:
             ko2en_model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M").to(device)
             en2ko_model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M").to(device)
 
+        if self.style == None:
+            style = None
+            style_transfer_pipeline = None
+        else:
+            style = self.style
+            style_checkpoint = "KoJLabs/bart-speech-style-converter"
+            style_tokenizer = AutoTokenizer.from_pretrained(style_checkpoint)
+            style_transfer_pipeline = pipeline('text2text-generation',model=style_checkpoint, tokenizer=style_tokenizer)
+
+
         return KoTANAugmentation(
             ko2en_tokenizer, en2ko_tokenizer,
-            ko2en_model, en2ko_model,
-            device,
+            ko2en_model, en2ko_model, 
+            style, style_transfer_pipeline,
+            device
         )
 
 
@@ -54,12 +67,16 @@ class KoTANAugmentation:
     def __init__(self, 
                  ko2en_tokenizer, en2ko_tokenizer,
                  ko2en_model, en2ko_model, 
+                 style, style_transfer_pipeline,
                  device):
         self.ko2en_tokenizer = ko2en_tokenizer
         self.en2ko_tokenizer = en2ko_tokenizer
 
         self.ko2en_model = ko2en_model
         self.en2ko_model = en2ko_model
+
+        self.style = style
+        self.style_transfer_pipeline = style_transfer_pipeline
 
         self.device = device
 
@@ -80,7 +97,12 @@ class KoTANAugmentation:
         # en2ko
         backtranslation = self._translate(translation, 'kor_Hang', self.en2ko_tokenizer, self.en2ko_model)
 
-        return backtranslation
+        # style transfer
+        if self.style_transfer_pipeline != None:
+            style_transfer = self._style_transfer(backtranslation, self.style_transfer_pipeline, self.style)
+            return style_transfer
+        else:
+            return backtranslation
 
 
     def _translate(self, text, tgt, tokenizer, model):
@@ -95,6 +117,15 @@ class KoTANAugmentation:
         translation = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)
         
         return translation
+    
+    def _style_transfer(self, text, pipeline, style):
+        text = [f"{style} 형식으로 변환:{sentence}" for sentence in text]
+        out = pipeline(text, max_length=100)
+        out = [ sentence['generated_text'] for sentence in out ]
+        return out
+
+
+
 
 
         
